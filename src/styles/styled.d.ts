58d84ebb866e3daa9ddc9e0c1bd222ce5a578fd9
juliocesarrodriguespiau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-05-23 19:14:44 -0300

    commit

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,23 @@
+//recurso para sobrescrever tipos de arquivos
+
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        title: string;
+
+        color: {
+            primary: string;
+            secondary: string;
+            tertiary: string;
+    
+            white: string;
+            black: string;
+            gray: string;
+    
+            success: string;
+            info: string;
+            warning: string;
+        },
+    };
+}
