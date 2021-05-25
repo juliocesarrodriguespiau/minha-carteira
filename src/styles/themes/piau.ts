db4fab43593ed90e016158dd03b39aa8b546b334
juliocesarrodriguespiau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-05-24 22:03:25 -0300

    commit menu lateral

---
+++ src/styles/themes/piau.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    title: 'piau',
+
+    colors: {
+        primary: '#1B263B',
+        secondary: '#415A77',
+        tertiary: '#778DA9',
+
+        white: '#FFF',
+        black: '#000',
+        gray: '#757575',
+
+        success: '#76FF03',
+        info: '#1976D2',
+        warning: '#F50057',
+    },
+};
