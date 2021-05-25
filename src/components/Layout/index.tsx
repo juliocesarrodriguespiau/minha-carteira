e5948919a92f30eee579ef4b3b4d7c92ff457f7f
juliocesarrodriguespiau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2021-05-25 00:11:45 -0300

    commit content header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,14 +6,18 @@ import MainHeader from '../MainHeader';
 import Aside from '../Aside';
 import Content from '../Content';
 
-const Layout: React.FC = () => {
+const Layout: React.FC = ({children}) => {
     return (
         <Grid>
             <MainHeader />
             <Aside />
-            <Content />
+            <Content>
+                {children}
+            </Content>
         </Grid>
     );
 }
 
-export default Layout;
+export default Layout;   
+
+
